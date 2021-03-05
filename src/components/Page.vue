a3468c361317af68f0f7d256a251adebeb96d33c
DESKTOP-02J98EU\Asus <arisa6845@gmail.com>
2021-03-05 15:45:09 +0800

    add new footer

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -89,36 +89,7 @@
           </div>
         </div>
         <div class="col-10">
-          <div class="footer mt-5 position-relative">
-            <div class="footer-content position-absolute w-100">
-              <div class="section-title-swag d-none d-sm-flex align-items-center justify-content-center">
-                <img src="@/assets/img/swagleft.svg" class="grass-left">
-                <p>Say Hi</p>
-                <img src="@/assets/img/swagright.svg" class="grass-right">
-              </div>
-              <div class="mt-2 mt-md-4">
-                <h2 class="text-center">聯絡我們</h2>
-                <div class="mt-2 mt-sm-3 mt-lg-5 position-absolute footer-items">
-                  <div class="d-flex align-items-center">
-                    <font-awesome-icon icon="envelope" />
-                    <span class="ms-3">hello@gmail.com</span>
-                  </div>
-                  <div class="d-flex align-items-center mt-3">
-                    <font-awesome-icon icon="phone" />
-                    <span class="ms-3">0912 111 123</span>
-                  </div>
-                  <div class="d-flex mt-3">
-                    <font-awesome-icon icon="map-marker-alt" />
-                    <div class="ms-3">
-                      <b>雙北都可約</b>
-                      <p>168 新北市幸福區幸福路二段520號, 台灣No.1</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <img src="@/assets/img/18e62af.png" alt="white-circle" class="footer-bg w-100 h-auto">
-          </div>
+          <Footer />
         </div>
       </div>
     </div>
@@ -126,6 +97,7 @@
 </template>
 
 <script>
+import Footer from './Footer';
 import db from '@/assets/page.json';
 
 export default {
@@ -135,6 +107,9 @@ export default {
       allData: [],
       data: {}
     }
+  },
+  components: {
+    Footer
   },
   created(){
     const copyData = JSON.parse(JSON.stringify(db.item));
